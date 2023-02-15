23a77f06bc976f1568f207b9f5d950e0674c5eb3
AugustPaula <101798634+AugustPa@users.noreply.github.com>
2023-02-15 17:05:25 +0100

    now one can change stripe size and rotation in angles

--- Assets/Scripts/GenerateStripes.cs
+++ Assets/Scripts/GenerateStripes.cs
@@ -4,27 +4,42 @@ using UnityEngine;
 
 public class GenerateStripes : MonoBehaviour
 {
-    public float radius = 1f;
+    public float stripeSize = 10f;
     public int numberOfObjects = 10;
     public GameObject prefab;
-    public float rotationSpeed = 10f;
-    public float initAngle = 0f;
-
+    List<GameObject> myObjects = new List<GameObject>();
+    public float angularSpeed = 15f;
     void Start()
     {
+        float radius = (1/(2*Mathf.Tan(stripeSize * Mathf.Deg2Rad)/2));
         float angleStep = 360f / numberOfObjects;
         for (int i = 0; i < numberOfObjects; i++)
         {
-            float angle = (i * angleStep) + initAngle;
+            float angle = (i * angleStep);
             float x = Mathf.Sin(angle * Mathf.Deg2Rad) * radius;
             float z = Mathf.Cos(angle * Mathf.Deg2Rad) * radius;
             Vector3 pos = new Vector3(x, 0, z);
-            Instantiate(prefab, pos, Quaternion.identity, transform);
+            Vector3 direction = Vector3.Normalize(Vector3.zero - pos);
+            GameObject newObj = Instantiate(prefab, pos, Quaternion.LookRotation(direction), transform);
+            myObjects.Add(newObj);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        float radius = (1/(2*Mathf.Tan(stripeSize * Mathf.Deg2Rad)/2));
+        float angleStep = 360f / numberOfObjects;
+        for (int i = 0; i< numberOfObjects; i++)
+        {
+            GameObject obj = myObjects[i];
+            float angle = (i * angleStep) + (angularSpeed * Time.fixedTime);
+            float x = Mathf.Sin(angle * Mathf.Deg2Rad) * radius;
+            float z = Mathf.Cos(angle * Mathf.Deg2Rad) * radius;
+            Vector3 newPosition = new Vector3(x, 0, z);
+            obj.transform.position = newPosition;
+            Vector3 direction = Vector3.Normalize(Vector3.zero - newPosition);
+            obj.transform.rotation = Quaternion.LookRotation(direction);
+        }
     }
 }
