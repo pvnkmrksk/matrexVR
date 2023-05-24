c9544bb05d80e036643e8816b8550a5752b357f5
pavan Kaushik <pkaushik@ab.mpg.de>
2023-05-24 18:00:06 +0200

    data log the drum pose and sensor data

--- Assets/Scripts/ZmqListener.cs
+++ Assets/Scripts/ZmqListener.cs
@@ -2,23 +2,16 @@ using UnityEngine;
 using NetMQ;
 using NetMQ.Sockets;
 using System.Threading;
-
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
-
-// using UnityEngine.SceneManagement;
 
 public class ZmqListener : MonoBehaviour
 {
     private readonly string address = "tcp://localhost:9872"; // Replace with your socket address
     private SubscriberSocket subscriber;
     private string message; // The message received from the socket
-    private Vector3 positionToUpdate;
-    private Quaternion rotationToUpdate;
-    private string logPath;
-    private StreamWriter logFile;
+    public Vector3 positionToUpdate { get; private set; }
+    public Quaternion rotationToUpdate { get; private set; }
 
     void Start()
     {
@@ -36,7 +29,7 @@ public class ZmqListener : MonoBehaviour
                     string topic = subscriber.ReceiveFrameString();
                     message = subscriber.ReceiveFrameString();
 
-                    // Update the position based on the received values
+                    // Update the position and rotation based on the received values
                     string[] values = message.Split(',');
 
                     // Transform the position
@@ -56,59 +49,6 @@ public class ZmqListener : MonoBehaviour
                 }
             }
         }).Start();
-    }
-
-    void InitLog()
-    {
-        string date = DateTime.Now.ToString("yyyy-MM-dd");
-        string time = DateTime.Now.ToString("HH-mm-ss");
-        string directoryPath = Path.Combine(Application.persistentDataPath, "data", date);
-        Directory.CreateDirectory(directoryPath); // Creates the directory if it doesn't exist
-        logPath = Path.Combine(directoryPath, $"{date}_{time}.csv");
-        logFile = new StreamWriter(logPath);
-        logFile.WriteLine(
-            "Current Time,DrumPosX, DrumPosY, DrumPosZ, DrumRotX,DrumRotY,DrumRotZ,SensPosX, SensPosY, SensPosZ, SensRotX,SensRotY,SensRotZ"
-        );
-        Debug.Log("Writing data to: " + logPath);
-    }
-
-    void Update()
-    {
-        // //get the transform of the drum
-        // drumTransform = GameObject.Find("GratingDrum").transform;
-
-        // // Check if 'values' array is populated before updating the position and rotation
-        // if (positionToUpdate != null)
-        // {
-        //     transform.position = positionToUpdate;
-        //     transform.rotation = rotationToUpdate;
-        // }
-
-        // string currentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
-        // float DrumPosX = drumTransform.position.x;
-        // float DrumPosY = drumTransform.position.y;
-        // float DrumPosZ = drumTransform.position.z;
-        // float DrumRotX = drumTransform.rotation.x;
-        // float DrumRotY = drumTransform.rotation.y;
-        // float DrumRotZ = drumTransform.rotation.z;
-        // float SensPosX = positionToUpdate[0];
-        // float SensPosY = positionToUpdate[1];
-        // float SensPosZ = positionToUpdate[2];
-        // float SensRotX = rotationToUpdate[0];
-        // float SensRotY = rotationToUpdate[1];
-        // float SensRotZ = rotationToUpdate[2];
-
-        // string line =
-        //     $"{currentTime},{DrumPosX},{DrumPosY},{DrumPosZ},{DrumRotX},{DrumRotY},{DrumRotZ},{SensPosX},{SensPosY},{SensPosZ},{SensRotX},{SensRotY},{SensRotZ}";
-
-        // // string line =
-        // //     $"{currentTime},{xPos},{zPos},{yRot},{stripeStartTime},{angularSpeed},{sceneName}";
-        // logFile.WriteLine(line);
-        // Debug.Log(line);
-
-        // // print the message received from the socket
-        // Debug.Log(positionToUpdate);
-        // Debug.Log(rotationToUpdate);
     }
 
     void OnDestroy()
