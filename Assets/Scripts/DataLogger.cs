c860bf1247067a572616b9d8595146a4b1064211
pavan Kaushik <pkaushik@ab.mpg.de>
2023-05-25 15:49:28 +0200

    log async with buffering and compress the stream

--- Assets/Scripts/DataLogger.cs
+++ Assets/Scripts/DataLogger.cs
@@ -1,26 +1,42 @@
 using UnityEngine;
-using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
+using System;
+using System.IO.Compression;
+using System.Text;
+using System.Threading.Tasks;
 
 public class DataLogger : MonoBehaviour
 {
     private string logPath;
     private StreamWriter logFile;
+    private List<string> bufferedLines;
+    private bool isLogging;
+    private bool isBuffering;
 
     void Start()
     {
         InitLog();
+        bufferedLines = new List<string>();
+        isLogging = true;
+        isBuffering = true;
+
+        StartCoroutine(FlushBufferedLinesRoutine());
     }
 
     void InitLog()
     {
         string date = DateTime.Now.ToString("yyyy-MM-dd");
         string time = DateTime.Now.ToString("HH-mm-ss");
-        string rootDirectoryPath = Application.dataPath; // Root directory of the project or build
+        string rootDirectoryPath = Application.dataPath;
         string directoryPath = Path.Combine(rootDirectoryPath, "data", date);
-        Directory.CreateDirectory(directoryPath); // Creates the directory if it doesn't exist
-        logPath = Path.Combine(directoryPath, $"{date}_{time}.csv");
-        logFile = new StreamWriter(logPath);
+        Directory.CreateDirectory(directoryPath);
+        logPath = Path.Combine(directoryPath, $"{date}_{time}.csv.gz");
+        logFile = new StreamWriter(
+            new GZipStream(File.Create(logPath), System.IO.Compression.CompressionLevel.Optimal)
+        );
+
         logFile.WriteLine(
             "Current Time,DrumPosX,DrumPosY,DrumPosZ,DrumRotX,DrumRotY,DrumRotZ,SensPosX,SensPosY,SensPosZ,SensRotX,SensRotY,SensRotZ"
         );
@@ -43,12 +59,55 @@ public class DataLogger : MonoBehaviour
             + $"{drumRotation.eulerAngles.y},{drumRotation.eulerAngles.z},{sensPosition.x},{sensPosition.y},"
             + $"{sensPosition.z},{sensRotation.eulerAngles.x},{sensRotation.eulerAngles.y},{sensRotation.eulerAngles.z}";
 
-        logFile.WriteLine(line);
-        logFile.Flush();
+        if (isBuffering)
+        {
+            bufferedLines.Add(line);
+        }
+        else
+        {
+            WriteLogLine(line);
+        }
     }
 
     void OnDestroy()
     {
-        logFile.Close();
+        isLogging = false;
+        isBuffering = false;
+        logFile?.Dispose();
+    }
+
+    async Task FlushBufferedLines()
+    {
+        var linesToWrite = new List<string>(bufferedLines);
+        bufferedLines.Clear();
+
+        foreach (var line in linesToWrite)
+        {
+            WriteLogLine(line);
+        }
+
+        await logFile.FlushAsync();
+    }
+
+    IEnumerator FlushBufferedLinesRoutine()
+    {
+        while (isLogging)
+        {
+            if (bufferedLines.Count > 0)
+            {
+                yield return FlushBufferedLines();
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+    }
+
+    void WriteLogLine(string line)
+    {
+        byte[] lineBytes = Encoding.UTF8.GetBytes(line);
+        logFile.BaseStream.Write(lineBytes, 0, lineBytes.Length);
+        logFile.BaseStream.WriteByte((byte)'\n');
     }
 }
