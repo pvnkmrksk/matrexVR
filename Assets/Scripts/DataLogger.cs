11c1110688ab8538ff52f230fa6276c775d0f653
Pavan Kumar Kaushik <pkaushik@ab.mpg.de>
2023-10-20 15:17:08 +0200

    add master datalogging system datalogger base class logs the attached gameobject pose and zmq data.
    scene logger overrides and appends any extra cols.
    
    master data collectes all the loggers and stores it in one directory

--- Assets/Scripts/DataLogger.cs
+++ Assets/Scripts/DataLogger.cs
@@ -1,99 +1,87 @@
-using UnityEngine;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using System.IO.Compression;
-using System.Text;
-using System.Threading.Tasks;
-
-public class DataLogger : MonoBehaviour
+public abstract class DataLogger : MonoBehaviour
 {
-    private string directoryPath;
-    private string logPath;
-    private StreamWriter logFile;
-    private List<string> bufferedLines;
-    private bool isLogging;
-    private bool isBuffering;
-    private bool isFirstLine;
+    protected string directoryPath;
+    protected string logPath;
+    protected StreamWriter logFile;
+    protected List<string> bufferedLines;
+    protected bool isLogging;
+    protected bool isBuffering;
+    protected bool isFirstLine;
+    protected ZmqListener zmq;
 
-    private ZmqListener zmq;
-
-    void Start()
+    protected virtual void Start()
     {
-        Debug.Log("Start() is executed");
         MasterDataLogger masterDataLogger = FindObjectOfType<MasterDataLogger>();
         if (masterDataLogger != null)
         {
-            Debug.Log("Found MasterDataLogger");
             directoryPath = masterDataLogger.directoryPath;
-            Debug.Log("directoryPath:" + directoryPath);
             bufferedLines = new List<string>();
             isLogging = true;
             isBuffering = true;
             isFirstLine = true;
-
-            InitLog();
-
-            StartCoroutine(FlushBufferedLinesRoutine());
 
             zmq = GetComponent<ZmqListener>();
             if (zmq == null)
             {
                 Debug.LogError("ZmqListener component not found in the GameObject. Please attach ZmqListener script to the GameObject.");
             }
+
+            InitLog();
+            StartCoroutine(FlushBufferedLinesRoutine());
         }
         else
         {
             Debug.LogError("MasterDataLogger not found.");
         }
     }
-    void InitLog()
+    protected virtual void InitLog()
     {
         string date = DateTime.Now.ToString("yyyy-MM-dd");
         string time = DateTime.Now.ToString("HH-mm-ss");
         string gameObjectName = this.gameObject.name; // Get the name of the GameObject the script is attached to
-        string rootDirectoryPath = Application.dataPath;
-        Debug.Log("init directory path:" + directoryPath);
-        // Add GameObject name in the log filename
         logPath = Path.Combine(directoryPath, $"{date}_{time}_{gameObjectName}_.csv.gz");
         
         logFile = new StreamWriter(
             new GZipStream(File.Create(logPath), System.IO.Compression.CompressionLevel.Optimal)
         );
 
-        // Write the header row
-        logFile.WriteLine(
-            "Current Time,VR,Scene,SensPosX,SensPosY,SensPosZ,SensRotX,SensRotY,SensRotZ,InsectPosX,InsectPosY,InsectPosZ,InsectRotX,InsectRotY,InsectRotZ"
+        // Write the header row without a newline character at the end
+        logFile.Write(
+            "Current Time,VR,Scene,SensPosX,SensPosY,SensPosZ,SensRotX,SensRotY,SensRotZ,GameObjectPosX,GameObjectPosY,GameObjectPosZ,GameObjectRotX,GameObjectRotY,GameObjectRotZ"
         );
 
         Debug.Log("Writing data to: " + logPath);
     }
 
-    void Update()
+protected virtual void Update()
+{
+    if (zmq == null)
+        return;
+
+    // Collect the necessary information
+    string currentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
+    string vr = this.gameObject.name; // The name of the GameObject this script is attached to
+    string scene = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name; // The name of the current scene
+
+    Vector3 sensPosition = zmq.pose.position;
+    Quaternion sensRotation = zmq.pose.rotation;
+
+    // The current position and rotation of the GameObject this script is attached to
+    Vector3 gameObjectPosition = this.transform.position;
+    Quaternion gameObjectRotation = this.transform.rotation;
+
+    // Log the data
+    string line =
+        $"\n{currentTime},{vr},{scene},{sensPosition.x},{sensPosition.y},{sensPosition.z},"
+        + $"{sensRotation.eulerAngles.x},{sensRotation.eulerAngles.y},{sensRotation.eulerAngles.z},"
+        + $"{gameObjectPosition.x},{gameObjectPosition.y},{gameObjectPosition.z},{gameObjectRotation.eulerAngles.x},"
+        + $"{gameObjectRotation.eulerAngles.y},{gameObjectRotation.eulerAngles.z}";
+
+    LogData(line);
+}
+
+    protected virtual void LogData(string line)
     {
-        if (zmq == null)
-            return;
-
-        // Collect the necessary information
-        string currentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
-        string vr = this.gameObject.name; // The name of the GameObject this script is attached to
-        string scene = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name; // The name of the current scene
-
-        Vector3 sensPosition = zmq.pose.position;
-        Quaternion sensRotation = zmq.pose.rotation;
-
-        // The current position and rotation of the GameObject this script is attached to
-        Vector3 insectPosition = this.transform.position;
-        Quaternion insectRotation = this.transform.rotation;
-
-        // Log the data
-        string line =
-            $"{currentTime},{vr},{scene},{sensPosition.x},{sensPosition.y},{sensPosition.z},"
-            + $"{sensRotation.eulerAngles.x},{sensRotation.eulerAngles.y},{sensRotation.eulerAngles.z},"
-            + $"{insectPosition.x},{insectPosition.y},{insectPosition.z},{insectRotation.eulerAngles.x},"
-            + $"{insectRotation.eulerAngles.y},{insectRotation.eulerAngles.z}";
-
         if (isBuffering)
         {
             if (isFirstLine)
@@ -110,45 +98,46 @@ public class DataLogger : MonoBehaviour
             WriteLogLine(line);
         }
     }
-        void OnDestroy()
+
+    protected virtual void OnDestroy()
+    {
+        isLogging = false;
+        isBuffering = false;
+        logFile?.Dispose();
+    }
+
+    async Task FlushBufferedLines()
+    {
+        var linesToWrite = new List<string>(bufferedLines);
+        bufferedLines.Clear();
+
+        foreach (var line in linesToWrite)
         {
-            isLogging = false;
-            isBuffering = false;
-            logFile?.Dispose();
+            await logFile.WriteLineAsync(line);
         }
 
-        async Task FlushBufferedLines()
-        {
-            var linesToWrite = new List<string>(bufferedLines);
-            bufferedLines.Clear();
+        await logFile.FlushAsync();
+    }
 
-            foreach (var line in linesToWrite)
+    IEnumerator FlushBufferedLinesRoutine()
+    {
+        while (isLogging)
+        {
+            if (bufferedLines.Count > 0)
             {
-                await logFile.WriteLineAsync(line);
+                yield return FlushBufferedLines();
             }
-
-            await logFile.FlushAsync();
-        }
-
-        IEnumerator FlushBufferedLinesRoutine()
-        {
-            while (isLogging)
+            else
             {
-                if (bufferedLines.Count > 0)
-                {
-                    yield return FlushBufferedLines();
-                }
-                else
-                {
-                    yield return null;
-                }
+                yield return null;
             }
         }
+    }
 
-        void WriteLogLine(string line)
-        {
-            byte[] lineBytes = Encoding.UTF8.GetBytes(line);
-            logFile.BaseStream.Write(lineBytes, 0, lineBytes.Length);
-            logFile.BaseStream.WriteByte((byte)'\n');
-        }
+    void WriteLogLine(string line)
+    {
+        byte[] lineBytes = Encoding.UTF8.GetBytes(line);
+        logFile.BaseStream.Write(lineBytes, 0, lineBytes.Length);
+        logFile.BaseStream.WriteByte((byte)'\n');
+    }
 }
--- Assets/Scripts/MasterDataLogger.cs
+++ Assets/Scripts/MasterDataLogger.cs
@@ -1,19 +1,32 @@
-using System.IO;
-using System.IO.Compression;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MasterDataLogger : MonoBehaviour
 {
     public string directoryPath { get; private set; }
-    //todo: add data loggers for scene based data
-    //todo: add metadata files such as json files for scene based data
-    
+    private List<DataLogger> dataLoggers;
+
     void Start()
     {
         Debug.Log("Start() is executed");
         string timestamp = System.DateTime.Now.ToString("yyyyMMddHHmmss");
         directoryPath = Application.dataPath + "/RunData/" + timestamp;
         Directory.CreateDirectory(directoryPath);
+
+        // Find all DataLogger instances in the scene
+        dataLoggers = new List<DataLogger>();
+        foreach (var logger in FindObjectsOfType<DataLogger>())
+        {
+            if (logger.gameObject.scene == gameObject.scene)
+            {
+                dataLoggers.Add(logger);
+            }
+        }
+        // Initialize all DataLogger instances
+        foreach (var logger in dataLoggers)
+        {
+            logger.InitLog();
+        }
     }
 
     public void ZipDataFolder()
