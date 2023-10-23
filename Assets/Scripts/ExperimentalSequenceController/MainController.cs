08fdbdb9a135a48e4584e4d2768cd4cccbf73e35
Pavan Kumar Kaushik <17527035+pvnkmrksk@users.noreply.github.com>
2023-10-23 02:28:26 +0200

    Revert "Make masterdatalogger a singleton Accidental duplicate instances are severed on creation to prevent confusions can be accessed by  MasterDataLogger.Instance from anywhere"
    This reverts commit 32a2db65930313fb5ce7002ef1417b3e5114c869.

--- Assets/Scripts/ExperimentalSequenceController/MainController.cs
+++ Assets/Scripts/ExperimentalSequenceController/MainController.cs
@@ -30,17 +30,15 @@ public class MainController : MonoBehaviour
         DontDestroyOnLoad(this.gameObject);
 
 
-        // Access the MasterDataLogger instance
-        
-        masterDataLogger = MasterDataLogger.Instance;
-
+        // Find the MasterDataLogger component
+        masterDataLogger = GetComponentInChildren<MasterDataLogger>();
         if (masterDataLogger == null)
         {
-            Logger.Log("MasterDataLogger instance not found", 1);
+            Logger.Log("MasterDataLogger not found on the GameObject or its children.", 1);
         }
         else
         {
-            Logger.Log("MasterDataLogger instance found.", 3);
+            Logger.Log("MasterDataLogger found on the GameObject or its children.", 3);
             Logger.Log("MasterDataLogger.directoryPath: " + masterDataLogger.directoryPath, 4);
         }
 
--- Assets/Scripts/MasterDataLogger.cs
+++ Assets/Scripts/MasterDataLogger.cs
@@ -7,35 +7,16 @@ using System.IO.Compression;
 // Manages all data loggers in the scene
 public class MasterDataLogger : MonoBehaviour
 {
-    // Singleton instance
-    public static MasterDataLogger Instance { get; private set; }
-
     // Path to the directory where the log files will be saved
     public string directoryPath { get; private set; }
 
     // List of all DataLogger instances in the scene
     private List<DataLogger> dataLoggers;
 
-    // Create timestamp variable that can be publicly accessed but not changed with get methods only
+    //cretae timestamp  variable that can be publicly accessed but not changed with get methods only
     public string timestamp { get; private set; }
 
-    void Awake()
-    {
-        /*          the Awake() method sets Instance to this only if Instance is null. 
-        If Instance is not null, it remains the same. This ensures that only one instance 
-        of MasterDataLogger exists in your game at any time.
-         */
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else if (Instance != this)
-        {
-            Destroy(gameObject);
-        }
-    }
-    // d at the start of the scene
+    // Called at the start of the scene
     void Start()
     {
         // Get the current timestamp
