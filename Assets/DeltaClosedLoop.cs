4607291623a7e8aecd0d899d558894cc114b7608
AugustPaula <101798634+AugustPa@users.noreply.github.com>
2023-11-06 18:32:06 +0100

    Add DeltaClosedLoop script for incremental updates
    Introduce a new DeltaClosedLoop.cs script designed to update GameObjects based on the delta of position and rotation since the last frame. This approach replaces the previous method of using absolute coordinates from FicTrac, allowing for more efficient and responsive object transformations. The script retains all functionalities of the existing ClosedLoop script and is intended to supersede it once fully tested and confirmed for stability.

---
+++ Assets/DeltaClosedLoop.cs
@@ -0,0 +1,171 @@
+using UnityEngine;
+
+public class DeltaClosedLoop : MonoBehaviour
+{
+    [Header("Initial Settings")]
+    public Vector3 initialPosition = Vector3.zero;
+    public Vector3 initialRotation = Vector3.zero;
+
+    [Header("FicTrac Settings")]
+    public float sphereRadius = 5.0f; // Default value for sphere radius in centimeters
+
+    [Header("Gain Settings")]
+    [SerializeField, Range(0, 1000)]
+    private float xGain = 100.0f;
+    [SerializeField, Range(0, 1000)]
+    private float yGain = 100.0f;
+    [SerializeField, Range(0, 1000)]
+    private float zGain = 100.0f;
+    [SerializeField, Range(0, 1000)]
+    private float rollGain = 100.0f;
+    [SerializeField, Range(0, 1000)]
+    private float pitchGain = 100.0f;
+    [SerializeField, Range(0, 1000)]
+    private float yawGain = 100.0f;
+
+    [Header("Loop Settings")]
+    [Tooltip("Toggle closed loop orientation.")]
+    public bool closedLoopOrientation = true;
+    [Tooltip("Toggle closed loop position.")]
+    public bool closedLoopPosition = true;
+
+    // Placeholder structure for the sensor data
+    private struct SensorData
+    {
+        public float x;
+        public float y;
+        public float z;
+        public float roll;
+        public float pitch;
+        public float yaw;
+    }
+
+    private SensorData lastSensorData;
+    private SensorData currentSensorData;
+
+    // Reference to the ZmqListener component
+    private ZmqListener _zmqListener;
+
+    // Position and Rotation Offsets
+    private Vector3 posOffset = Vector3.zero;
+    private Quaternion rotOffset = Quaternion.identity;
+
+    private void Start()
+    {
+        _zmqListener = GetComponent<ZmqListener>();
+
+        // Set initial sensor data
+        UpdateSensorData();
+
+        // Apply the initial position to the transform
+        transform.position = initialPosition;
+        transform.rotation = Quaternion.Euler(initialRotation);
+    }
+
+    private void Update()
+    {
+        HandleInput();
+        UpdateSensorData();
+        ApplyDeltaTransformations();
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            ToggleClosedLoopOrientation();
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ToggleClosedLoopPosition();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ResetPosition();
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            ResetRotation();
+        }
+    }
+
+    private void UpdateSensorData()
+    {
+        if (_zmqListener.pose != null)
+        {
+            // Update current sensor data from the ZmqListener
+            currentSensorData.x = _zmqListener.pose.position.x;
+            currentSensorData.y = _zmqListener.pose.position.y;
+            currentSensorData.z = _zmqListener.pose.position.z;
+            currentSensorData.pitch = _zmqListener.pose.rotation.eulerAngles.x;
+            currentSensorData.yaw = _zmqListener.pose.rotation.eulerAngles.y;
+            currentSensorData.roll = _zmqListener.pose.rotation.eulerAngles.z;
+        }
+    }
+
+    private void ApplyDeltaTransformations()
+    {
+           if (closedLoopPosition)
+        {
+            // Calculate position deltas
+        Vector3 positionDelta = new Vector3(
+            currentSensorData.y - lastSensorData.y,
+            -(currentSensorData.z - lastSensorData.z), // Unity's Y is up, sensor's Z is up (negative Unity Y)
+            currentSensorData.x - lastSensorData.x
+        );
+
+        // Apply the gains and sphereRadius scaling to position delta
+        positionDelta = new Vector3(positionDelta.x * yGain, positionDelta.y * -zGain, positionDelta.z * xGain) * sphereRadius;
+
+        // Apply position deltas
+        transform.position += positionDelta;
+        }
+           if (closedLoopOrientation)
+        {
+        // Calculate rotation deltas
+        Vector3 rotationDelta = new Vector3(
+            currentSensorData.pitch - lastSensorData.pitch, // Pitch - rotation around X-axis
+            currentSensorData.yaw - lastSensorData.yaw,     // Yaw - rotation around Y-axis
+            currentSensorData.roll - lastSensorData.roll    // Roll - rotation around Z-axis
+        );
+
+        // Apply the gains to rotation delta
+        rotationDelta = new Vector3(rotationDelta.x * pitchGain, rotationDelta.y * yawGain, rotationDelta.z * rollGain);
+
+        // Apply rotation deltas as torque
+        transform.Rotate(rotationDelta.x, rotationDelta.y, rotationDelta.z, Space.Self);
+
+        // Update lastSensorData for the next frame
+        lastSensorData = currentSensorData;
+        }
+    }
+
+    public void ResetPosition()
+    {
+        transform.position = initialPosition;
+        lastSensorData = currentSensorData;
+    }
+
+    public void ResetRotation()
+    {
+        transform.rotation = Quaternion.Euler(initialRotation);
+        lastSensorData = currentSensorData;
+ 
+    }
+    private void ToggleClosedLoopOrientation()
+    {
+        closedLoopOrientation = !closedLoopOrientation;
+    }
+
+    private void ToggleClosedLoopPosition()
+    {
+        closedLoopPosition = !closedLoopPosition;
+    }
+    
+}
+
+
+    // Additional functionality as needed...
