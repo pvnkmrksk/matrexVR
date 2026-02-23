4003c4074f0d2ec43e6635f50c2e50e79986ecc2
August Paula <apaula@ab.mpg.de>
2026-02-23 10:14:59 +0100

    directednessVSDecisionAccuracy experiment

---
+++ Assets/Scripts/BuildDirectednessVSDecisionAccuracyDesign.cs
@@ -0,0 +1,236 @@
+#if UNITY_EDITOR
+using System.IO;
+using Newtonsoft.Json;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildDirectednessVSDecisionAccuracyDesign
+{
+    [MenuItem("Tools/Generate Directedness VS Decision Accuracy SequenceDesign.json")]
+    private static void Build()
+    {
+        var intertrial = new
+        {
+            name = "skybox",
+            trigger = new { type = "time", seconds = 5 },
+            closedLoopOrientation = true,
+            closedLoopPosition = true,
+            objects = new[]
+            {
+                new
+                {
+                    type = "glassplane",
+                    polar = new { radius = 0, angle = 0, height = -1 }
+                }
+            },
+            camera = new[]
+            {
+                new { vrId = "VR1", clearFlags = "Skybox" },
+                new { vrId = "VR2", clearFlags = "Skybox" },
+                new { vrId = "VR3", clearFlags = "Skybox" },
+                new { vrId = "VR4", clearFlags = "Skybox" }
+            }
+        };
+
+        var barTrigger = new
+        {
+            type = "area",
+            areaTag = "Goal",
+            vrId = "any",
+            shape = "cylinder",
+            size = 5f,
+            radius = 2.5f,
+            height = 5f,
+            timeoutSeconds = 120f
+        };
+
+        var solidCamera = new[]
+        {
+            new { vrId = "VR1", clearFlags = "SolidColor", bgColor = new[] { 0.8f, 0.8f, 0.8f, 1f } },
+            new { vrId = "VR2", clearFlags = "SolidColor", bgColor = new[] { 0.8f, 0.8f, 0.8f, 1f } },
+            new { vrId = "VR3", clearFlags = "SolidColor", bgColor = new[] { 0.8f, 0.8f, 0.8f, 1f } },
+            new { vrId = "VR4", clearFlags = "SolidColor", bgColor = new[] { 0.8f, 0.8f, 0.8f, 1f } }
+        };
+
+        var black = new[] { 0f, 0f, 0f, 1f };
+        var gray04 = new[] { 0.4f, 0.4f, 0.4f, 1f };
+
+        var scale = new { x = 7, y = 100, z = 7 };
+
+        var steps = new object[]
+        {
+            new
+            {
+                name = "Decision_singleBlack_60cm_0deg",
+                trigger = barTrigger,
+                closedLoopOrientation = true,
+                closedLoopPosition = true,
+                randomInitialRotation = false,
+                objects = new[]
+                {
+                    new
+                    {
+                        type = "ScalingCylinder",
+                        polar = new { radius = 60, angle = 0, height = 0 },
+                        material = "SetColor",
+                        color = black,
+                        scale,
+                        visualAngleDegrees = 10
+                    }
+                },
+                camera = solidCamera
+            },
+            new
+            {
+                name = "Decision_twoBlack_60cm_40deg_noflip",
+                trigger = barTrigger,
+                closedLoopOrientation = true,
+                closedLoopPosition = true,
+                randomInitialRotation = false,
+                objects = new[]
+                {
+                    new
+                    {
+                        type = "ScalingCylinder",
+                        polar = new { radius = 60, angle = -20, height = 0 },
+                        material = "SetColor",
+                        color = black,
+                        scale,
+                        visualAngleDegrees = 10
+                    },
+                    new
+                    {
+                        type = "ScalingCylinder",
+                        polar = new { radius = 60, angle = 20, height = 0 },
+                        material = "SetColor",
+                        color = black,
+                        scale,
+                        visualAngleDegrees = 10
+                    }
+                },
+                camera = solidCamera
+            },
+            new
+            {
+                name = "Decision_twoBlack_60cm_40deg_flip",
+                trigger = barTrigger,
+                closedLoopOrientation = true,
+                closedLoopPosition = true,
+                randomInitialRotation = false,
+                objects = new[]
+                {
+                    new
+                    {
+                        type = "ScalingCylinder",
+                        polar = new { radius = 60, angle = 20, height = 0 },
+                        material = "SetColor",
+                        color = black,
+                        scale,
+                        visualAngleDegrees = 10
+                    },
+                    new
+                    {
+                        type = "ScalingCylinder",
+                        polar = new { radius = 60, angle = -20, height = 0 },
+                        material = "SetColor",
+                        color = black,
+                        scale,
+                        visualAngleDegrees = 10
+                    }
+                },
+                camera = solidCamera
+            },
+            new
+            {
+                name = "Decision_blackGray04_60cm_40deg_noflip",
+                trigger = barTrigger,
+                closedLoopOrientation = true,
+                closedLoopPosition = true,
+                randomInitialRotation = false,
+                objects = new[]
+                {
+                    new
+                    {
+                        type = "ScalingCylinder",
+                        polar = new { radius = 60, angle = -20, height = 0 },
+                        material = "SetColor",
+                        color = black,
+                        scale,
+                        visualAngleDegrees = 10
+                    },
+                    new
+                    {
+                        type = "ScalingCylinder",
+                        polar = new { radius = 60, angle = 20, height = 0 },
+                        material = "SetColor",
+                        color = gray04,
+                        scale,
+                        visualAngleDegrees = 10
+                    }
+                },
+                camera = solidCamera
+            },
+            new
+            {
+                name = "Decision_Gray04black_60cm_40deg_flip",
+                trigger = barTrigger,
+                closedLoopOrientation = true,
+                closedLoopPosition = true,
+                randomInitialRotation = false,
+                objects = new[]
+                {
+                    new
+                    {
+                        type = "ScalingCylinder",
+                        polar = new { radius = 60, angle = -20, height = 0 },
+                        material = "SetColor",
+                        color = gray04,
+                        scale,
+                        visualAngleDegrees = 10
+                    },
+                    new
+                    {
+                        type = "ScalingCylinder",
+                        polar = new { radius = 60, angle = 20, height = 0 },
+                        material = "SetColor",
+                        color = black,
+                        scale,
+                        visualAngleDegrees = 10
+                    }
+                },
+                camera = solidCamera
+            },
+            new
+            {
+                name = "Decision_emptyWorld_20s",
+                trigger = new { type = "time", seconds = 20 },
+                closedLoopOrientation = true,
+                closedLoopPosition = true,
+                randomInitialRotation = false,
+                objects = new object[] { },
+                camera = solidCamera
+            }
+        };
+
+        var design = new
+        {
+            seed = -1,
+            repetitions = 1,
+            sync = true,
+            intertrial,
+            steps
+        };
+
+        string path = Path.Combine(
+            Application.streamingAssetsPath,
+            "sequenceDesign_directednessVSDecisionAccuracy.json"
+        );
+
+        Directory.CreateDirectory(Application.streamingAssetsPath);
+        File.WriteAllText(path, JsonConvert.SerializeObject(design, Formatting.Indented));
+
+        Debug.Log($"Wrote design -> {path}");
+        AssetDatabase.Refresh();
+    }
+}
+#endif
